68c304d32217848005b1162f1ce1e45e8f2ecda0
mina-heidary <mina_heidary@yahoo.com>
2025-11-20 00:24:11 +0330

    Remove extra file

--- ui/setup_icons.py
+++
@@ -1,8 +0,0 @@
-from ui.settings import SettingIcon
-
-def create_setting_icons():
-    return [
-        SettingIcon("volume", "data/menu/botton/icon/volume.png", (30, 30), 105),
-        SettingIcon("shop", "data/menu/botton/icon/shop.png", (32, 32), 10),
-        SettingIcon("bag", "data/menu/botton/icon/bag.png", (50, 50), 50)
-    ]
